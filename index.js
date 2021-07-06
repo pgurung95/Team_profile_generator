bb38b52fcc78463bb43096a4c444c27ad1bd2092
Prashant Gurung <prashantgurung@Prashants-MacBook-Pro.local>
2021-07-05 20:02:43 -0400

    created main files

---
+++ index.js
@@ -0,0 +1,10 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+
+menu = () => {
+    create
+}
